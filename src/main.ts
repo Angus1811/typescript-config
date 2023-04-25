c733e58a25bb9c3cd629a0c1fe9000d8038d5fb4
Angus.C <102298861+Angus1811@users.noreply.github.com>
2023-04-25 20:18:45 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,1 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server);
-
-server.listen(3000, () => {console.log('now listening on:http://localhost:3000')});
+console.log("hello world")
